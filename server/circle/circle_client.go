4707b14da79347841cf264110a3a9fb4ec0d457c
Umang Parmar <umangjparmar@gmail.com>
2020-09-28 11:39:08 +0530

    add helper fn around circleci-v2 client library

--- server/circle/circle_client.go
+++ server/circle/circle_client.go
@@ -6,6 +6,15 @@ import (
 	"github.com/darkLord19/circleci-v2/circleci"
 )
 
+var (
+	client *circleci.APIClient
+)
+
+func init() {
+	config := circleci.NewConfiguration()
+	client = circleci.NewAPIClient(config)
+}
+
 func getClient() *circleci.APIClient {
 	config := circleci.NewConfiguration()
 	return circleci.NewAPIClient(config)
@@ -19,11 +28,30 @@ func getContext(apiToken string) context.Context {
 
 // GetCurrentUser returns the current user
 func GetCurrentUser(apiToken string) (*circleci.User, error) {
-	client := getClient()
 	user, _, err := client.UserApi.GetCurrentUser(getContext(apiToken))
 	if err != nil {
 		return nil, err
 	}
 
 	return &user, nil
+}
+
+// GetWorkflow returns the info for given workflow id
+func GetWorkflow(apiToken string, workflowID string) (*circleci.Workflow, error) {
+	wf, _, err := client.WorkflowApi.GetWorkflowById(getContext(apiToken), workflowID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &wf, nil
+}
+
+// GetWorkflowJobs returns the info of jobs for given workflow id
+func GetWorkflowJobs(apiToken string, workflowID string) (*[]circleci.Job, error) {
+	wf, _, err := client.WorkflowApi.ListWorkflowJobs(getContext(apiToken), workflowID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &wf.Items, nil
 }
