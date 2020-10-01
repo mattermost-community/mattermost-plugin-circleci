f54c8e9eff2a6c54c3bdbec499ccde880b0c622b
Nathanaël <contact@nathanaelhoun.fr>
2020-10-01 13:25:28 +0200

    Update webhooks

--- server/store/subscriptions.go
+++ server/store/subscriptions.go
@@ -112,11 +112,14 @@ func (s *Subscriptions) GetSubscriptionsByChannel(channelID string) []*Subscript
 	return filteredSubs
 }
 
+func (s *Subscriptions) GetSubscriptionsForRepository(owner, repository string) []*Subscription {
+	key := v1.GetFullNameFromOwnerAndRepo(owner, repository)
+	return s.Repositories[key]
+}
+
 // Return a list of subscribed channel IDs
 func (s *Subscriptions) GetSubscribedChannelsForRepository(owner, repository string) []string {
-	key := v1.GetFullNameFromOwnerAndRepo(owner, repository)
-
-	subs := s.Repositories[key]
+	subs := s.GetSubscriptionsForRepository(owner, repository)
 	if subs == nil {
 		return nil
 	}
@@ -124,6 +127,23 @@ func (s *Subscriptions) GetSubscribedChannelsForRepository(owner, repository str
 	var channelIDs []string
 	for _, sub := range subs {
 		channelIDs = append(channelIDs, sub.ChannelID)
+	}
+
+	return channelIDs
+}
+
+func (s *Subscriptions) GetFilteredChannelsForBuild(owner, repository string, isFailed bool) []string {
+	subs := s.GetSubscriptionsForRepository(owner, repository)
+	if subs == nil {
+		return nil
+	}
+
+	var channelIDs []string
+	for _, sub := range subs {
+		switch { // nolint:gocritic // It's expected that more flags get added.
+		case isFailed || !sub.Flags.OnlyFailedBuilds:
+			channelIDs = append(channelIDs, sub.ChannelID)
+		}
 	}
 
 	return channelIDs
