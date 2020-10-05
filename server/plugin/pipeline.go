88e3b4ec45cec205c21a13ea1d9a203a77d2525d
Umang Parmar <umangjparmar@gmail.com>
2020-10-05 12:56:26 +0530

    fix build failure

--- server/plugin/pipeline.go
+++ server/plugin/pipeline.go
@@ -264,11 +264,9 @@ func (p *Plugin) executeTriggerPipeline(args *model.CommandArgs, token string,
 	project *store.ProjectIdentifier, split []string) (*model.CommandResponse, *model.AppError) {
 	var params circleci.TriggerPipelineParameters
 	if len(split) <= 0 {
-		return p.sendEphemeralResponse(args, "Please choose branch or tag subcommand to trigger pipeline"), nil		
+		return p.sendEphemeralResponse(args, "Please choose branch or tag subcommand to trigger pipeline"), nil
 	}
- 		
 	subcmd := split[0]
-	}
 	input := ""
 
 	switch subcmd {
