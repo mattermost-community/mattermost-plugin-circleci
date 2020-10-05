fed6a9dfc29536f6fe4fce671a33a9ce663985c3
Nathanaël <contact@nathanaelhoun.fr>
2020-10-05 09:52:19 +0200

    Resolve lint issue

--- server/plugin/pipeline.go
+++ server/plugin/pipeline.go
@@ -263,7 +263,7 @@ func (p *Plugin) executePipelineGetWorkflowByID(args *model.CommandArgs,
 func (p *Plugin) executeTriggerPipeline(args *model.CommandArgs, token string,
 	project *store.ProjectIdentifier, split []string) (*model.CommandResponse, *model.AppError) {
 	var params circleci.TriggerPipelineParameters
-	if len(split) <= 0 {
+	if len(split) == 0 {
 		return p.sendEphemeralResponse(args, "Please choose branch or tag subcommand to trigger pipeline"), nil
 	}
 	subcmd := split[0]
