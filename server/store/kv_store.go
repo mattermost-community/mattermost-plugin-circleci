3f5e21cb5bff97f718590086ea33b0cfff26aff5
Nathanaël Houn <contact@nathanaelhoun.fr>
2021-03-05 11:45:46 +0100

    Moved kv_store suffixes to prefixes

--- server/store/kv_store.go
+++ server/store/kv_store.go
@@ -8,14 +8,14 @@ import (
 )
 
 const (
-	storeTokenSuffix          = "_circleci_token"  // Full key format is userID_circleci_token
-	defaultProjectStoreSuffix = "_default_project" // Full key format is userID_default_project
+	storeTokenPrefix          = "circleci_token_"  // Full key format is circleci_token_userID
+	defaultProjectStorePrefix = "default_project_" // Full key format is default_project_userID
 	subscriptionsKVKey        = "subscriptions"
 )
 
 // GetTokenForUser returns false if no token is saved for this user
 func (s *Store) GetTokenForUser(userID, encryptionKey string) (string, bool) {
-	raw, appErr := s.api.KVGet(userID + storeTokenSuffix)
+	raw, appErr := s.api.KVGet(storeTokenPrefix + userID)
 	if appErr != nil {
 		s.api.LogError("Unable to reach KVStore", "KVStore error", appErr)
 		return "", false
@@ -42,7 +42,7 @@ func (s *Store) StoreTokenForUser(userID, circleciToken, encryptionKey string) b
 		return false
 	}
 
-	appErr := s.api.KVSet(userID+storeTokenSuffix, []byte(encryptedToken))
+	appErr := s.api.KVSet(storeTokenPrefix+userID, []byte(encryptedToken))
 	if appErr != nil {
 		s.api.LogError("Unable to write in KVStore", "KVStore error", appErr)
 		return false
@@ -53,7 +53,7 @@ func (s *Store) StoreTokenForUser(userID, circleciToken, encryptionKey string) b
 
 // DeleteTokenForUser return sfalse if the token has not been deleted
 func (s *Store) DeleteTokenForUser(userID string) bool {
-	if appErr := s.api.KVDelete(userID + storeTokenSuffix); appErr != nil {
+	if appErr := s.api.KVDelete(storeTokenPrefix + userID); appErr != nil {
 		s.api.LogError("Unable to delete from KVStore", "KVStore error", appErr)
 		return false
 	}
@@ -100,7 +100,7 @@ func (s *Store) StoreSubscriptions(subs *Subscriptions) error {
 func (s *Store) GetDefaultProject(userID string) (*ProjectIdentifier, error) {
 	var pi *ProjectIdentifier
 
-	savedDefaultProject, err := s.api.KVGet(userID + defaultProjectStoreSuffix)
+	savedDefaultProject, err := s.api.KVGet(defaultProjectStorePrefix + userID)
 	if err != nil {
 		s.api.LogError("Unable to get default project", err)
 		return nil, errors.Wrap(err, "Unable to get default project")
@@ -124,7 +124,7 @@ func (s *Store) StoreDefaultProject(userID string, project ProjectIdentifier) er
 		return errors.Wrap(err, "error while converting project to json")
 	}
 
-	if err := s.api.KVSet(userID+defaultProjectStoreSuffix, projectBytes); err != nil {
+	if err := s.api.KVSet(defaultProjectStorePrefix+userID, projectBytes); err != nil {
 		s.api.LogError("Unable to save default project", err)
 		return errors.Wrap(err, "Unable to save default project")
 	}
